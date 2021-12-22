0c165f5ae16fa444827fef4db8ccaf4131baa978
Viskal Bayu <vbayua@gmail.com>
2021-12-22 10:57:28 +0700

    added get data for community

--- api/controllers/Community.js
+++ api/controllers/Community.js
@@ -1,13 +1,30 @@
-const validation = require('express-validator')
+// const validation = require('express-validator')
 const Community = require('../models/Community')
 
-module.exports.list = (request, response, next) {
-  Community.find({}, (error, articles) => {
+module.exports.list = (request, response, next) => {
+  Community.find({}, (error, community) => {
     if (error) {
       return response.status(500).json({
         message: 'Failed to retrieve data.'
       })
     }
-    return response.json()
+    return response.json(community)
+  })
+}
+
+module.exports.getCommunityById = (request, response) => {
+  const id = request.params.id
+  Community.findOne({ _id: id }, (error, community) => {
+    if (error) {
+      return response.status(500).json({
+        message: 'Failed to retrieve community data'
+      })
+    }
+    if (!community) {
+      return response.status(404).json({
+        message: 'Community not found!'
+      })
+    }
+    return response.json(community)
   })
 }
