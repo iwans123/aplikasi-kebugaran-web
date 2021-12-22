f8d7a4b3f0f4c70ed0b200a0e1c3f8d9b8c6a3b8
Viskal Bayu <vbayua@gmail.com>
2021-12-22 11:17:42 +0700

    small fixes to card

--- components/Dashboard/Community/Search.vue
+++ components/Dashboard/Community/Search.vue
@@ -33,10 +33,10 @@
           <img src="images/Home/workout.svg" class="card-img-top" alt="...">
           <div class="card-body">
             <h5 class="card-title">
-              Card title
+              <!--  -->
             </h5>
             <p class="card-text">
-              Some quick example text to build on the card title and make up the bulk of the card's content.
+              <!-- Some quick example text to build on the card title and make up the bulk of the card's content. -->
             </p>
             <a href="#" class="btn btn-primary">Go somewhere</a>
           </div>
