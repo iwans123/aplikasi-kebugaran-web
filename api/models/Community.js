b392a269eedd61e20682308a24658643533d8f9a
Viskal Bayu <vbayua@gmail.com>
2021-12-22 11:01:07 +0700

    Modify Community Schema for detailed info

--- api/models/Community.js
+++ api/models/Community.js
@@ -4,6 +4,8 @@ const Schema = mongoose.Schema
 const Community = new Schema({
   name: { type: String, required: true },
   city: { type: String, required: true },
+  description: { type: String },
+  contactInfo: { type: String },
   location: { type: String, required: true },
   schedule: { type: String, required: true }
 })
