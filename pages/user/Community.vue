4cb75114d59d49e0a1da05bd674a2ea3f97ea296
Viskal Bayu <vbayua@gmail.com>
2021-12-23 00:28:18 +0700

    Updated community page for retrieving data

--- pages/user/Community.vue
+++ pages/user/Community.vue
@@ -1,14 +1,39 @@
 <template>
-  <div>
-    <app-community />
+  <div id="wrapper">
+    <app-sidebar />
+    <div id="content-wrapper" class="d-flex flex-column">
+      <div id="content">
+        <app-navbartop />
+        <app-search :communities="communities" />
+      </div>
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; AksiKalistenik 2021</span>
+          </div>
+        </div>
+      </footer>
+    </div>
   </div>
 </template>
 <script>
-import Community from '@/components/Dashboard/Community/Community.vue'
+// import Community from '@/components/Dashboard/Community/Community.vue'
+import Sidebar from '@/components/Dashboard/Sidebar.vue'
+import NavbarTop from '@/components/Dashboard/NavbarTop.vue'
+import Search from '@/components/Dashboard/Community/Search.vue'
 
 export default {
   components: {
-    'app-community': Community
+    // 'app-community': Community
+    'app-sidebar': Sidebar,
+    'app-navbartop': NavbarTop,
+    'app-search': Search
+  },
+  async asyncData (context) {
+    const { data } = await context.$axios.get('/api/community-data')
+    return {
+      communities: data
+    }
   }
 }
 </script>
