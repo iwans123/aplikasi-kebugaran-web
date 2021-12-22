99c441b34d3d34533ee5148c14321974e0840cc3
Viskal Bayu <vbayua@gmail.com>
2021-12-22 11:16:25 +0700

    Added instagram field

--- api/models/Community.js
+++ api/models/Community.js
@@ -6,6 +6,7 @@ const Community = new Schema({
   city: { type: String, required: true },
   description: { type: String },
   contactInfo: { type: String },
+  instagram: { type: String },
   location: { type: String, required: true },
   schedule: { type: String, required: true }
 })
