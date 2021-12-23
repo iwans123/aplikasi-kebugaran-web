9e0e7be886a486be3900f1d92b9017afe8195dd3
Viskal Bayu <vbayua@gmail.com>
2021-12-24 05:46:07 +0700

    Added workout and exercise crud functions

--- api/controllers/Workout.js
+++ api/controllers/Workout.js
@@ -30,3 +30,82 @@ module.exports.create = (request, response) => {
     })
   })
 }
+
+module.exports.update = (request, response) => {
+  const id = request.params.id
+  const exerciseId = Math.floor(Math.random() * 1000)
+  const exerciseList = {
+    exid: exerciseId,
+    exname: request.body.exname,
+    exsets: request.body.exsets,
+    comment: request.body.comment
+  }
+  Workout.findOne({ _id: id }, (error, workout) => {
+    if (error) {
+      return response.status(500).json({
+        message: 'Error updating record',
+        error
+      })
+    }
+    if (!workout) {
+      return response.status(404).json({
+        message: 'Workout not found'
+      })
+    }
+
+    workout.exercises.push(exerciseList)
+
+    workout.save((error, workout) => {
+      if (error) {
+        return response.status(500).json({
+          message: 'failed to save record'
+        })
+      }
+      return response.json(workout)
+    })
+  })
+}
+// Delete Workout
+module.exports.delete = (request, response) => {
+  const id = request.params.id
+  Workout.findByIdAndRemove(id, (error, workout) => {
+    if (error) {
+      return response.status(500).json({
+        message: 'Error getting record'
+      })
+    }
+    return response.json(workout)
+  })
+}
+// Delete Exercise
+module.exports.deleteex = (request, response) => {
+  const id = request.params.id
+  const exid = request.params.exid
+  Workout.findOne({ _id: id }, (error, workout) => {
+    if (error) {
+      return response.status(500).json({
+        message: 'Error updating record',
+        error
+      })
+    }
+    if (!workout) {
+      return response.status(404).json({
+        message: 'Workout not found'
+      })
+    }
+
+    const pos = workout.exercises.findIndex(el => el.exid === exid)
+    if (pos > 0) {
+      workout.exercises.splice(pos, 1)
+    }
+
+    workout.save((error, workout) => {
+      if (error) {
+        return response.status(500).json({
+          message: 'failed to save record'
+        })
+      }
+      return response.json(workout)
+    })
+  })
+}
--- api/routes/workout.js
+++ api/routes/workout.js
@@ -5,5 +5,8 @@ const workout = require('../controllers/Workout')
 
 route.get('/workout-data', workout.getWorkout)
 route.post('/workout-data/create', workout.create)
+route.put('/workout-data/update/:id', workout.update)
+route.delete('/workout-data/delete/:id', workout.delete)
+route.put('/workout-data/delete/:id/:exid', workout.deleteex)
 
 module.exports = route
